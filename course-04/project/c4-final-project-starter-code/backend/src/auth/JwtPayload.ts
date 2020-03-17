d57a6d98c465494b50d11e790df8213d3ce95142
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2020-03-17 14:55:49 +0100

    feature/add-authentication

--- course-04/project/c4-final-project-starter-code/backend/src/auth/JwtPayload.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/auth/JwtPayload.ts
@@ -7,3 +7,19 @@ export interface JwtPayload {
   iat: number
   exp: number
 }
+
+export interface IKey {
+  alg: string
+  kty: string
+  use: string
+  x5c: string[]
+  n: string
+  e: string
+  kid: string
+  x5t: string
+}
+
+export interface IFilteredKey {
+  kid: string
+  publicKey: string
+}
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -5,14 +5,14 @@ import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
+import { JwtPayload, IKey, IFilteredKey } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-g5wm-7ri.eu.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,12 +56,60 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if (!token) {
+    throw new Error('No token')
+  }
+
+  if (!token.toLowerCase().startsWith('bearer ')) {
+    throw new Error('Invalid token')
+  }
+
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const res = await Axios.get(jwksUrl)
+  const keys = filterKeys(res.data.keys)
+
+  if (!keys.length) {
+    throw new Error('No keys available for signing')
+  }
+
+  const cert = keys.filter((key: IFilteredKey) => {
+    if (jwt.header.kid === key.kid) {
+      return key
+    }
+  })[0]
+
+  if (!cert) {
+    throw new Error('No keys available for signing')
+  }
+
+  return verify(token, cert.publicKey, { algorithms: ['RS256'] })
+}
+
+function filterKeys(keys: IKey[]): IFilteredKey[] {
+  return keys
+    .filter((key: IKey) => {
+      if (
+        key.use === 'use' &&
+        key.kty === 'RSA' &&
+        key.kid &&
+        ((key.x5c && key.x5c.length) || (key.n && key.e))
+      ) {
+        return key
+      }
+    })
+    .map(
+      (key: IKey): IFilteredKey => ({
+        kid: key.kid,
+        publicKey: certToPEM(key.x5c[0])
+      })
+    )
+}
+
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n')
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return cert
 }
 
 function getToken(authHeader: string): string {
